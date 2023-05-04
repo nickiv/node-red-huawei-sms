50aaf4617cc1206c116d2cbe86579ce246df576b
Nickolay Ivannikov <nickiv@jivosite.com>
2023-05-04 11:35:17 +0300

    sending sms

---
+++ huawei-sms-send.js
@@ -0,0 +1,23 @@
+const api = require("./huawei-sms-api");
+
+module.exports = function(RED) {
+    function huaweiSmsSendNode(n) {
+        RED.nodes.createNode(this, n);
+        var node = this;
+        this.phone = n.phone;
+        this.config = RED.nodes.getNode(n.config);
+        node.on("input", function(msg, send, done){
+          var req = api.sendSms(node.config.ip, node.config.admin_password, node.phone, msg.payload);
+          req.on('success', function(resp){
+            msg.payload = resp;
+            send(msg);
+            done();
+          });
+          req.on('error', function(err){
+            done(err);
+          });
+        });
+    }
+
+    RED.nodes.registerType("huawei-sms-send", huaweiSmsSendNode);
+}
